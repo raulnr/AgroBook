eca589d1c99a36ee73594d6576d90ac508a49aee
raul_nunez <raulnuneztrabajo@gmail.com>
2018-12-23 11:11:02 +0100

    v0.0.0 Commit inicial con ficheros y carpetas

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='AgroBook',
+    description='Cuaderno de campo',
+    version='0.0.1',
+    author='raulnr',
+    author_email='raulnuneztrabajo@gmail.com',
+    packages=[
+        'rnr'
+    ],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    setup_requires=[],
+    tests_require=[]
+    )
